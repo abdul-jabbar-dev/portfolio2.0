18bebc40bc1fa21332c56d3bd2858509e0babe7c
abdul-jabbar-dev <abduljabbardev@gmail.com>
2025-10-15 02:32:20 +0600

    woring

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
- 
+
 const META = {
   OG_IMAGE: "https://res.cloudinary.com/dnkwv76h3/image/upload/fl_preserve_transparency/v1760386929/portfolio/meta-OG/abdulJabbarImg1.jpg?_s=public-apps",
 }
- 
 
-const CONFIG={
-  BACKEND_GRAPHQL_ENDPOINT:  (process.env.NODE_ENV === "development" ? process.env.PG_STRING : process.env.PG_STRING_PROD) || "http://localhost:8000/gql",
+
+const CONFIG = {
+  BACKEND_GRAPHQL_ENDPOINT: process.env.PG_STRING_PROD,
   META,
-  STORAGE: {TOKEN_KEY: "token"},
+  STORAGE: { TOKEN_KEY: "token" },
 }
-console.log(CONFIG,process.env.NODE_ENV );
+console.log(CONFIG, process.env);
 export default CONFIG;
