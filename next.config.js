9f25d30cb9518552a92cfe5e02c7d9b6acf16c4d
abdul-jabbar-dev <abduljabbardev@gmail.com>
2025-10-15 02:53:28 +0600

    woring

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  
+  experimental: {
+    runtime: 'nodejs',
+  },
   images: {
     domains: ["avatars.githubusercontent.com", "img.icons8.com","abduljabbar.netlify.app","*","www.smartaddons.com","res.cloudinary.com"],
   },
