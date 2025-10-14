5259996d087944e262e8328fe391bb4965d8ab6a
abdul-jabbar-dev <abduljabbardev@gmail.com>
2025-10-15 03:14:52 +0600

    D

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,14 @@
 const META = {
   OG_IMAGE: "https://res.cloudinary.com/dnkwv76h3/image/upload/fl_preserve_transparency/v1760386929/portfolio/meta-OG/abdulJabbarImg1.jpg?_s=public-apps",
 }
-
-const BACKEND_GRAPHQL_ENDPOINT: string =
-  process.env.NODE_ENV === "development"
+const isDev = false
+const BACKEND_GRAPHQL_ENDPOINT: string = isDev
     ? process.env.PG_STRING ?? "http://localhost:8000/gql"
     : process.env.PG_STRING_PROD ?? "https://abduljabbar-portfolio.deno.dev/gql";
+console.log(BACKEND_GRAPHQL_ENDPOINT);
 const CONFIG = {
   BACKEND_GRAPHQL_ENDPOINT,
   META,
   STORAGE: { TOKEN_KEY: "token" },
 }
-console.log(CONFIG, process.env);
 export default CONFIG;
