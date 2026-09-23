1a1da72bf7554709a41d6fe3cbf12ab877699c95
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2026-09-23 15:19:47 +0600

    Fix Netlify build: update plugin-nextjs and remove invalid experimental runtime config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    runtime: "nodejs",
-  },
   images: {
     unoptimized: true,
     domains: [
