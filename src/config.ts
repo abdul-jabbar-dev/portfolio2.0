f6e3783a70fbf8dba84d1b60ff77aa9695019d3a
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2026-09-23 14:42:07 +0600

    asfasf

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,7 @@ const META = {
 }
 const isDev = process.env.NODE_ENV !== "production";
 
-const rawEndpoint = isDev
-  ? process.env.PG_STRING ?? "http://localhost:8000"
-  : "https://portfolio-api-fhnyca9dqtjb.abdul-jabbar-dev.deno.net";
+const rawEndpoint = "https://portfolio-api-fhnyca9dqtjb.abdul-jabbar-dev.deno.net";
 
 const BACKEND_ENDPOINT: string = rawEndpoint.endsWith('/gql') ? rawEndpoint.slice(0, -4) : rawEndpoint;
 const BACKEND_GRAPHQL_ENDPOINT: string = BACKEND_ENDPOINT + "/gql";
