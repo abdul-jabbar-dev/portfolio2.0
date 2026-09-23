a97c5ba999458a985a1e2438177c3c596cdb2645
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2026-09-23 17:58:07 +0600

    asfasf

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const META = {
   OG_IMAGE: "https://res.cloudinary.com/dnkwv76h3/image/upload/fl_preserve_transparency/v1760386929/portfolio/meta-OG/abdulJabbarImg1.jpg?_s=public-apps",
 };
 
-const rawEndpoint = process.env.NEXT_PUBLIC_DENO_BACKEND_URL || process.env.DENO_BACKEND_URL || "https://portfolio-api-fhnyca9dqtjb.abdul-jabbar-dev.deno.net";
+const rawEndpoint = process.env.NEXT_PUBLIC_BACKEND_URL || process.env.BACKEND_URL || "https://portfolio-api-fhnyca9dqtjb.abdul-jabbar-dev.deno.net";
 
 const BACKEND_ENDPOINT: string = rawEndpoint.endsWith('/gql') ? rawEndpoint.slice(0, -4) : rawEndpoint;
 const BACKEND_GRAPHQL_ENDPOINT: string = BACKEND_ENDPOINT + "/gql";
