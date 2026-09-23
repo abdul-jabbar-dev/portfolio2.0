010cddee1951718a13be7650ac56228a5bfd6665
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2026-09-23 20:34:38 +0600

    Update config to support both Netlify and local environments

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,10 @@
 const META = {
   OG_IMAGE: "https://res.cloudinary.com/dnkwv76h3/image/upload/fl_preserve_transparency/v1760386929/portfolio/meta-OG/abdulJabbarImg1.jpg?_s=public-apps",
 };
+ 
 
-const rawEndpoint = "https://pyoaowwarxwvraghsvpz.supabase.co/functions/v1";
-
-const BACKEND_ENDPOINT: string = "https://pyoaowwarxwvraghsvpz.supabase.co/functions/v1/gql";
-const BACKEND_GRAPHQL_ENDPOINT: string = "https://pyoaowwarxwvraghsvpz.supabase.co/functions/v1/gql";
+const BACKEND_ENDPOINT: string = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
+const BACKEND_GRAPHQL_ENDPOINT: string = BACKEND_ENDPOINT + "/gql";
 
 const CONFIG = {
   BACKEND_GRAPHQL_ENDPOINT,
